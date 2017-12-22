161b810436c7f54f5a4488acda03e78bd23b6d71
wenchengxiaopenyou@163.com <wenchengxiaopenyou@163.com>
2017-12-22 11:44:28 +0800

    core.py  json  load possition_file

--- lib/core.py
+++ lib/core.py
@@ -2,8 +2,6 @@
 # coding: utf-8
 # author  :zhaowencheng
 # desc    :check_flume
-import os
-import time
 import json
 from tool import *
 
@@ -14,20 +12,33 @@ position_dir = '/data0/flume/positionFile/'
 def check_pro(ps_cmd):
     ex_result = ex_cmd(ps_cmd)
     status = ex_result[2]
-    if status != 0:
-        allert_mail('SUDA前端服务器:'+ip+'flume进程不存在 请检查')
-    else:
-        pass
+    return status
 
-def diff_file(model):
+def get_last_file(model):
     position_file = position_dir + model + '_position.json'
     with open(position_file,'r') as f:
         data = json.load(f,encoding='utf-8')
-        print data[5]["file"]    #{u'inode': 922009, u'pos': 55574166, u'file': u'/data0/logs/clickstream/staytime.20171221160000'}
-        print data[5]["pos"]
+        return [data[5]["inode"],data[5]["pos"],data[5]["file"]]  #{u'inode': 922009, u'pos': 55574166, u'file': u'/data0/logs/clickstream/staytime.20171221160000'}
+
+def check_size(model):
+    size_ngixn_cmd = "du -sb " + get_last_file(model)[2]
+    size_flume_pos,size_nginx_log = get_last_file(model)[1],ex_cmd(size_ngixn_cmd)
+    return [size_nginx_log, size_flume_pos]
+
+def main(ps_cmd,):
+    status = check_pro(ps_cmd)
+    if status != 0:
+        allert_mail('SUDA前端服务器:'+ip+'flume进程不存在 请检查')
+        exit(127)
+    else:
+        model = 'staytime'
+        size_list = check_size(model)
+        print size_list
 
 
 
-diff_file('staytime')
+
+
+
 
 
