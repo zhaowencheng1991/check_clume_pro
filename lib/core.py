577e5d8270bf2af2795e8aff86236114e5f045a5
wenchengxiaopenyou@163.com <wenchengxiaopenyou@163.com>
2017-12-22 17:38:20 +0800

    core.py add ThreadPool.

--- lib/core.py
+++ lib/core.py
@@ -48,8 +48,8 @@ def diff_model_allert(model,p):
 
     #print model + ": size_flume_pos :", size_list["size_flume_pos"], "  size_nginx_log:", size_list["size_nginx_log"], "diff_num:",size_list["diff_num"]
     if size_list["diff_num"] >= allert_num:
-        print model,"同步延迟超过",allert_num,"B延迟大小(日志实际大小-flume读取大小)为:",size_list["diff_num"],"B延迟读取文件:",size_list["last_file"]
         lock.acquire()
+        print model,"同步延迟超过",allert_num,"B延迟大小(日志实际大小-flume读取大小)为:",size_list["diff_num"],"B延迟读取文件:",size_list["last_file"]
         global read_err_model_list
         read_err_model_list.append(model)
         lock.release()
