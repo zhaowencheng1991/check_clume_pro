d7bb062c530069bd215221eaa1262abdd3401179
wenchengxiaopenyou@163.com <wenchengxiaopenyou@163.com>
2017-12-22 17:30:48 +0800

    core.py add ThreadPool.

--- lib/core.py
+++ lib/core.py
@@ -69,7 +69,8 @@ def main(ps_cmd,):
             obj = t(target=diff_model_allert, args=(i, pool))
             obj.start()
         if read_err_model_list:
-            print read_err_model_list
+            #print read_err_model_list
+            pass
 
 main("ps aux | grep flume|grep -v grep")
 
