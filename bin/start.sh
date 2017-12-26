d249d354c2df2f3cfe3bc69372ebf83476483a93
wenchengxiaopenyou@163.com <wenchengxiaopenyou@163.com>
2017-12-26 20:09:20 +0800

    add conf.py

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 python=`which python`
-file_path=`echo $0 |awk -F 'start.sh' '{print $1}'`
+
 #echo $file_path
+if [ $0 != start.sh ];then
+    file_path=`echo $0 |awk -F 'start.sh' '{print $1}'`
+else
+    file_path=$0
+fi
 python $file_path/../lib/core.py
