327bf77be099f34e5d63d511eeac0c013338f3ad
wenchengxiaopenyou@163.com <wenchengxiaopenyou@163.com>
2017-12-27 18:25:45 +0800

    add conf.py

--- conf/conf.py
+++ conf/conf.py
@@ -3,7 +3,7 @@
 # author  :zhaowencheng
 # desc    :distcp
 
-model_list = ['sima_mrt','clickmap','clickstream']
+model_list = ['sima_mrt','clickmap','clickstream','suda_video','staytime']
 position_dir = '/data0/flume/positionFile/'
 allert_num = 2000000    #单位B
 allert_users = 'wencheng'
